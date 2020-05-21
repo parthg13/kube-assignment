1b5becf7959d05b2dbf6a23d3a16fff77feee936
Parth <parth.gupta@iauro.com>
2020-05-21 18:40:18 +0530

    parth:Assignment 1 completed

---
+++ assignment1/node_server/index.js
@@ -0,0 +1,30 @@
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
+
+const start =async()=>{
+    const Server = new Hapi.Server({
+        host: "0.0.0.0",
+        port: 3000,
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    });
+    await Server.register(require('@hapi/inert'));
+    Server.route({
+        path:'/',
+        method:"GET",
+        handler:(req, res)=>{
+            return res.file('./static.html')
+        }
+    })
+    
+    Server.start().then((res)=>{
+        console.log(`Server started at ${Server.info.uri}`)
+    }).catch((err)=>{
+        console.log("Server Error:",err);
+    })
+}
+
+start();
